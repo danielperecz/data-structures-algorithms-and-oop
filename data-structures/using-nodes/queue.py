f7f9e0a1177c1142c7adfda472cfa13cc8be2e16
Daniel <42898223+danielperecz@users.noreply.github.com>
2021-05-24 18:54:45 +0100

    Add files via upload

---
+++ data-structures/using-nodes/queue.py
@@ -0,0 +1,59 @@
+class Node:
+    
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+    def __repr__(self):
+        return str(self.data)
+
+class Queue:
+
+    def __init__(self):
+        self.first = None
+        self.last = None
+        self.size = 0
+
+    def __iter__(self):
+        node = self.first
+        while node is not None:
+            yield node
+            node = node.next
+
+    def __repr__(self):
+        nodes = []
+        for node in self:
+            nodes.append(str(node.data))
+        return " -> ".join(nodes)
+
+    def isEmpty(self):
+        return self.first is None
+
+    def length(self):
+        return self.size
+
+    # Add an item to the end of the list.
+    def add(self, item):
+        new_node = Node(item)
+        if self.last is not None:
+            self.last.next = new_node
+        
+        self.last = new_node
+        if self.first is None:
+            self.first = self.last
+
+        self.size += 1
+
+    # Remove the first item in the list.
+    def remove(self):
+        if not self.isEmpty():
+            item = self.first.data
+            self.first = self.first.next
+            if self.first is None:
+                self.last = None
+            self.size -= 1
+            return item
+
+    def peek(self):
+        if not self.isEmpty():
+            return self.first.data
---
+++ data-structures/using-nodes/stack.py
@@ -0,0 +1,49 @@
+class Node:
+    
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+    def __repr__(self):
+        return str(self.data)
+
+class Stack:
+
+    def __init__(self):
+        self.top = None
+        self.size = 0
+
+    def __iter__(self):
+        node = self.top
+        while node is not None:
+            yield node
+            node = node.next
+
+    def __repr__(self):
+        nodes = []
+        for node in self:
+            nodes.append(str(node.data))
+        return " -> ".join(nodes)
+
+    def isEmpty(self):
+        return self.top is None
+
+    def length(self):
+        return self.size
+
+    def push(self, item):
+        new_node = Node(item)
+        new_node.next = self.top
+        self.top = new_node
+        self.size += 1
+
+    def pop(self):
+        if not self.isEmpty():
+            item = self.top.data
+            self.top = self.top.next
+            self.size -= 1
+            return item
+
+    def peek(self):
+        if not self.isEmpty():
+            return self.top.data
