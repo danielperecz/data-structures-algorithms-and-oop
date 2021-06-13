f08436360a57647cfec9e7859a39217e93c89b42
pereczd2 <daniel.perecz2@mail.dcu.ie>
2021-06-13 13:29:44 +0100

    Renaming m to mid

--- algorithms/searching/binary_search.py
+++ algorithms/searching/binary_search.py
@@ -6,11 +6,11 @@ def binary_search(a, t):
         left = 0
         right = len(a) - 1
         while left != right:
-            m = math.ceil((left + right) / 2)
-            if a[m] > t:
-                right = m - 1
+            mid = math.ceil((left + right) / 2)
+            if a[mid] > t:
+                right = mid - 1
             else:
-                left = m
+                left = mid
         if a[left] == t:
             return left
     return "Unsuccessful"
