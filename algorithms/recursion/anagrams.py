d13583a7353fb6fd15c11fbf4b7c47a049ee91aa
pereczd2 <daniel.perecz2@mail.dcu.ie>
2021-08-01 20:00:51 +0100

    Implementing anagrams

---
+++ algorithms/recursion/anagrams.py
@@ -0,0 +1,9 @@
+def anagrams(s):
+    if len(s) == 1:
+        return [s]
+    else:
+        collection = []
+        for substring in anagrams(s[1:]):
+            for i in range(len(substring) + 1):
+                collection.append(substring[:i] + s[0] + substring[i:])
+        return collection
